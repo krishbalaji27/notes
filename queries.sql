a80e6efeb76ed97653467009f470252cf936d8aa
krishbalaji27 <krishbalaji1998@gmail.com>
2022-08-13 12:37:22 +0530

    inserted values,quries

--- queries.sql
+++ queries.sql
@@ -31,3 +31,13 @@ REFERENCES topics(id) ON UPDATE CASCADE ON DELETE CASCADE,
 constraint userId_ckn_1 FOREIGN KEY (userId)
 REFERENCES user_details(id) ON UPDATE CASCADE ON DELETE CASCADE
  );
+
+ 13/08/22
+
+insert into topics values (null,'Data Structures' , Null);
+insert into topics values (null, 'Array' , 1);
+select * from topics;
+insert into topicActivation values (null,1,1);
+insert into topicActivation values (null,2,1);
+insert into content values (null,2,'Definition','it is the simplest data structure where each data element can be accessed directly by only using its index number.',1);
+select * from content;
